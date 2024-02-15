c469abae085fbee9751d639675de5b22a35835c9
Albert Maršík <albert.marsik@gmail.com>
2024-02-15 19:28:29 +0100

    convproof polished even more; todo check $ and move on to convform

--- convproof/main.py
+++ convproof/main.py
@@ -3,7 +3,7 @@ import json
 import jsonschema
 from jsonschema import validate
 from .proof_exception import ProofException
-from .proof_references.main import proof_references
+from .proof_references import proof_references
 
 def validate_flow(path, flow, return_flow=False):
     issues = list()
@@ -11,21 +11,39 @@ def validate_flow(path, flow, return_flow=False):
     schema = dict()
     full_path = f"./{path}/{flow.lower()}.json"
 
+    # check valid path
     if not os.path.exists(full_path):
         issues.append("invalid path")
     else:
         with open(full_path, "r") as b:
-            bot = json.loads(b.read())
+            try:
+                bot = json.loads(b.read())
+            except json.JSONDecodeError as e:
+                issues.append(e)
+
+        if issues:
+           raise ProofException("JSON", issues)
+
         with open("convproof/schema.json", "r") as s:
             schema = json.loads(s.read())
 
+        # check structure and non-empty fields
         validator = jsonschema.Draft7Validator(schema)
         [issues.append(e) for e in validator.iter_errors(bot)]
 
+        if issues:
+           raise ProofException("structure", issues)
+
+        # check references
         proof_references(bot, issues)
 
         if issues:
-           raise ProofException(issues)
+            raise ProofException("references",issues)
+
+        issues.append("todo check that there isnt only $")
+
+        if issues:
+           raise ProofException("essence",issues)
         if return_flow:
             return bot
         else:
