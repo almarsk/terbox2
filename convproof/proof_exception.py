23cd6a8aa86b81d834f85f5849922f95d05fd317
Albert Maršík <albert.marsik@gmail.com>
2024-02-15 19:13:17 +0100

    convproof polished

--- convproof/proof_exception.py
+++ convproof/proof_exception.py
@@ -4,5 +4,5 @@ from posixpath import join
 class ProofException(Exception):
     def __init__(self, issues):
         newline = '\n'
-        self.message = f"\nflow is not flawless:\n{newline.join(str(issue) for issue in issues)}"
+        self.message = f"\n\nflow is not flawless:\n\n{newline.join(str(issue) for issue in issues)}"
         super().__init__(self.message)
--- convproof/proof_references/main.py
+++ convproof/proof_references/main.py
@@ -20,15 +20,21 @@ valid references
 """
 
 to_check = {
-    "flow": {"track": "states", "coda": "states"},
+    "flow": {
+        "track": "states",
+        "coda": "states"
+    },
     "states": {
         "context_intents": "intents",
         "context_states": "states",
         "iterate_states": "states",
-        "intents": {
-            "name": "intents",
-            "adjacent": "states"
-        }
+
+        # taking care of this separately
+        #
+        #"intents": {
+        #    "intents": "states",
+        #}
+
     },
     "intents": {
         "adjacent": "states",
@@ -38,53 +44,61 @@ to_check = {
     }
 }
 
+
 def proof_references(bot, issues):
-    issues.append("brah")
     intent_names = [intent["name"] for intent in bot["intents"]]
     state_names = [state["name"] for state in bot["states"]]
 
-    destinations_states = list()
-    destinations_intents = list()
+    destinations_states = set()
+    destinations_intents = set()
     for key, value in to_check.items():
         collect_destinations(key, value, "states", destinations_states)
         collect_destinations(key, value, "intents", destinations_intents)
 
-    for destination in destinations_states:
-        check_items = collect_check_items(destination, "states", bot)
-        [issues.append(f"missing {missing} in {destination}") for missing in filter(lambda i: i not in state_names, check_items)]
+    for destination in destinations_intents:
+        check_intents = collect_check_items(destination, "intents", bot)
+        [issues.append(f"missing intent {missing} from {', '.join(destination)}") for missing in filter(lambda i: i not in intent_names, check_intents)]
 
-    issues.append(f"Destinations to check for intents: {destinations_intents}")
+    issues.extend(f"missing intent {intent} from state {state['name']}" for state in bot["states"] for intent in state["intents"].keys() if intent not in intent_names)
+
+    for destination in destinations_states:
+        check_states = collect_check_items(destination, "states", bot)
+        [issues.append(f"missing state {missing} from {', '.join(destination)}") for missing in filter(lambda i: i not in state_names, check_states)]
+
+    issues.extend(f"missing state {', '.join(adjacent)} from state {state['name']} and its intent {intent}" for state in bot["states"] for intent,adjacent in state["intents"].items() if adjacent not in state_names)
 
 
 def collect_destinations(key, value, target_key, destinations):
     if isinstance(value, dict):
         for k,v in value.items():
-            collect_destinations(sediment_key(key) + [k] ,v, target_key, destinations)
+            collect_destinations(sediment(key) + [k] ,v, target_key, destinations)
     elif isinstance(value, str):
         if value is target_key:
-            destinations.append(key)
+            destinations.add(tuple(key))
 
-def sediment_key(key):
+
+def sediment(key):
     if isinstance(key, str):
         return [key]
     else:
         return [*key]
 
+
 def collect_check_items(destination, check_against, bot):
     where = bot
-    went_through_list = False
     resulting_items = list()
     for i, checkpoint in enumerate(destination):
-        print(f"where in iteration {i+1}: {where}\n with checkpoint {checkpoint}\n")
         if checkpoint == "flow":
             pass
         elif isinstance(where, list):
-            went_through_list = True
-            pass
-        elif went_through_list:
-            for current_items in where[checkpoint]:
-                resulting_items = resulting_items + current_items
+            for current_item in where:
+                for inner_checkpoint in destination[i:]:
+                    where = current_item[inner_checkpoint]
+                resulting_items = resulting_items + sediment(where)
+                resulting_items = [f"{i} of {check_against[:-1]} {current_item['name']}" for i in resulting_items]
+            print(resulting_items)
+            return resulting_items
         else:
             where = where[checkpoint]
 
-    return resulting_items if went_through_list else where
+    return where
